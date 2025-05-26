eed06336ffaee800297535bde45f4b1bf0bcb977
pavan-9699 <22eg105a46@anurag.edu.in>
2025-05-26 19:13:20 +0530

    finished project

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({ origin :'*'}));
 
 
 
